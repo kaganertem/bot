86bb1d9fbc1437019754449254254e5faa0a4abc
kaganertem <ayse.unur88@gmail.com>
2025-12-06 11:51:12 +0300

    Initialize Discord bot with message handling

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from fonksiyon import gen_pass
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    
+    else:
+        await message.channel.send("değişikmiş: " )
+client.run("")
